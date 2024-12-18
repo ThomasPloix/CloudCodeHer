5ebc9831a37756e0415e1d1c91ddafca8ddb59d1
ThomasPloix <thomas.ploix@gmail.com>
2024-12-18 12:27:58 +0100

    Setupteris modif port

--- setup-tetris.sh
+++ setup-tetris.sh
@@ -14,7 +14,7 @@ sudo apt install -y docker.io
 
 # Étape 4 : Téléchargement de l'image Docker pour Tetris
 echo "Téléchargement de l'image Docker pour Tetris... "
-sudo docker pull uzyexe/tetris && sudo docker run -d -p 8080:80 --name tetris uzyexe/tetris
+sudo docker pull uzyexe/tetris && sudo docker run -d -p 80:80 --name tetris uzyexe/tetris
 
 # Fin du script
 echo "Script terminé avec succès. Vous pouvez accéder au jeu Tetris sur le port 8080."
